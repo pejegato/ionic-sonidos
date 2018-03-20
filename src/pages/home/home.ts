1aca50bbc54063cb3621424b2fcdd7a3d5107118
rodrigo <ramirezretamalrodrigo@gmail.com>
2018-03-19 23:27:00 -0300

    Integracion sonidos

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+
+import { ANIMALES } from "../../data/data.animales"
+import { Animal } from "../../interfaces/animal.interface"
 
 @Component({
   selector: 'page-home',
@@ -7,7 +9,27 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  //crea un array llamado animales de tipo any, inicializado como array vacio;
+  animales: Animal[] = [];
+
+  constructor(){
+    this.animales = ANIMALES.splice(0);
+  }
+
+  reproducir(animal:Animal){
+    console.log(animal);
+    let audio = new Audio();
+    audio.src=animal.audio;
+
+
+    audio.load();
+    audio.play();
+
+    animal.reproduciendo = true;
+    setTimeout(()=>animal.reproduciendo = false, animal.duracion * 1000);
+
+
+
 
   }
 
